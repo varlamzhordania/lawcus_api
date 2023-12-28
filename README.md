9771f2ed8a44c64f42403ebe88fce37f41efcd5a
AmirBigdeli <141164942+varlamzhordania@users.noreply.github.com>
2023-12-28 19:29:39 +0400

    improve project structure and main.py + add requirements.txt

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# lawcus_api
+# Lawcus Data Migration
+
+## Overview
+
+This project enables the migration of data from the Lawcus platform to an Oracle database. It consists of scripts to
+connect to the Lawcus API, retrieve data for various endpoints, and insert the data into corresponding Oracle database
+tables.
+
+## Prerequisites
+
+Ensure that you have the following installed:
+
+- Python 3.x (recommend 3.9+)
+- Oracle Instant Client (for database connection)
+- Dependencies listed in `requirements.txt`
+
+## Setup
+
+1. Install Python dependencies:
+
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+2. Set up Oracle Instant Client.
+
+3. Configure API access:
+
+   Replace placeholder values in `main.py` for `API_BASE_URL`, `API_ACCESS_TOKEN`, and `API_HEADERS` with your actual
+   Lawcus API details.
+
+4. Configure database connection:
+
+   Update database connection details (DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_SID) in `main.py` based on your
+   Oracle database.
+
+## Usage
+
+Run the main script to initiate data migration:
+
+```bash
+python main.py
+```
+
+The script connects to the Oracle database, creates tables for different data types, fetches data from Lawcus API
+endpoints, and inserts data into corresponding tables.
+
+## Project Structure
+
+- `main.py`: Main script for data migration.
+- `contacts.py`, `matters.py`, ...: Endpoint-specific scripts with functions for creating tables and inserting data.
+- `utils.py`: Utility functions used across different scripts.
+- `requirements.txt`: List of Python dependencies.
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import platform
 import oracledb
 import requests
+import logging
 from contacts import create_contact_table, insert_contacts_into_table
 from accounts import create_accounts_table, insert_accounts_into_table
 from interactions import create_interactions_table, insert_interactions_into_table
@@ -9,27 +10,16 @@ from leads import create_leads_table, insert_leads_into_table
 from matters import create_matters_table, insert_matters_into_table
 from tasks import create_tasks_table, insert_tasks_into_table
 
-# Placeholder database connection parameters
-DB_USERNAME = "root"  # Replace with your DB username
-DB_PASSWORD = "1234"  # Replace with your DB password
-DB_HOST = "localhost"  # Replace with your DB host
-DB_PORT = 1521
-DB_SID = "xe"  # Replace with your DB SID
-
-# Placeholder API endpoint and headers
-API_BASE_URL = "https://api.us.lawcus.com"
-API_ACCESS_TOKEN = ""  # Replace with your actual API access token
-API_HEADERS = {
-    "Authorization": f"Oauth Bearer {API_ACCESS_TOKEN}",
-    "Content-Type": "application/json",
-}
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
-def connect_to_database():
+def configure_oracle_client():
     """
-    Connect to Oracle database and return a cursor.
+    Configure the Oracle client with the appropriate library directory.
 
-    Replace the placeholder values with your actual database connection details.
+    Replace the placeholder values with your actual library directory.
     """
     d = None  # default suitable for Linux
     if platform.system() == "Darwin" and platform.machine() == "x86_64":  # macOS
@@ -37,25 +27,55 @@ def connect_to_database():
     elif platform.system() == "Windows":
         d = r"C:\oracle\instantclient_21_12"
     oracledb.init_oracle_client(lib_dir=d)
-    connection = oracledb.connect(user=DB_USERNAME, password=DB_PASSWORD, host=DB_HOST, port=DB_PORT, sid=DB_SID)
-    cursor = connection.cursor()
-    return cursor
 
 
-def make_api_request(endpoint, params=None):
+def connect_to_database(username, password, host, port, service_id):
+    """
+    Connect to Oracle database and return a cursor.
+
+    :param username: Database username
+    :param password: Database password
+    :param host: Database host
+    :param port: Database port
+    :param service_id: Database SID
+
+    :return: Database cursor
+    """
+    try:
+        configure_oracle_client()
+        connection = oracledb.connect(user=username, password=password, host=host, port=port, sid=service_id)
+        cursor = connection.cursor()
+        return cursor
+    except oracledb.DatabaseError as e:
+        logger.error(f"Error connecting to the database: {e}")
+        raise
+    except Exception as e:
+        logger.error(f"Error occurred during database connection: {e}")
+        raise
+
+
+def make_api_request(endpoint, params=None, base_url=None, headers=None, **kwargs):
     """
     Make a dynamic API request based on the provided endpoint.
 
-    Replace the placeholder API_BASE_URL and API_HEADERS with your actual API details.
-    """
-    url = f"{API_BASE_URL}/{endpoint}"
-    response = requests.get(url, headers=API_HEADERS, params=params)
+    :param endpoint: The API endpoint
+    :param params: Parameters to include in the request
+    :param base_url: Base URL for the API
+    :param headers: Headers for the request
+    :param kwargs: Additional arguments for requests.get
 
-    if response.status_code == 200:
+    :return: JSON data if the request is successful, None otherwise
+    """
+    url = f"{base_url}/{endpoint}" if base_url else endpoint
+    headers = headers or {}
+
+    try:
+        response = requests.get(url, headers=headers, params=params, **kwargs)
+        response.raise_for_status()  # Raises HTTPError for bad responses
         data = response.json()
         return data
-    else:
-        print(f"Error: Unable to fetch data from {endpoint}. Status code: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        logger.error(f"Error making API request to {endpoint}: {e}")
         return None
 
 
@@ -65,8 +85,30 @@ def get_api_token():
 
 
 if __name__ == "__main__":
+
+    # Placeholder database connection parameters
+    DB_USERNAME = "root"  # Replace with your DB username
+    DB_PASSWORD = "1234"  # Replace with your DB password
+    DB_HOST = "localhost"  # Replace with your DB host
+    DB_PORT = 1521
+    DB_SID = "xe"  # Replace with your DB SID
+
+    # Placeholder API endpoint and headers
+    API_BASE_URL = "https://api.us.lawcus.com"
+    API_ACCESS_TOKEN = ""  # Replace with your actual API access token
+    API_HEADERS = {
+        "Authorization": f"Oauth Bearer {API_ACCESS_TOKEN}",
+        "Content-Type": "application/json",
+    }
+
     # Connect to the Oracle database
-    oracle_cursor = connect_to_database()
+    oracle_cursor = connect_to_database(
+        username=DB_USERNAME,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT,
+        service_id=DB_SID
+    )
 
     # Define the endpoints and parameters you need to call
     endpoints = [
@@ -82,7 +124,7 @@ if __name__ == "__main__":
 
     for endpoint, params in endpoints:
         # Make API request for the current endpoint
-        endpoint_data = make_api_request(endpoint, params)
+        endpoint_data = make_api_request(endpoint=endpoint, params=params, base_url=API_BASE_URL, headers=API_HEADERS)
 
         if endpoint_data:
             # Execute the corresponding function based on the endpoint
