d19ef2edfbe3c436aea0004fb9cbbc5bf27271fc
AmirBigdeli <141164942+varlamzhordania@users.noreply.github.com>
2023-12-28 16:12:12 +0400

    improve main.py and add insert functions

--- main.py
+++ main.py
@@ -10,11 +10,19 @@ from matters import create_matters_table, insert_matters_into_table
 from tasks import create_tasks_table, insert_tasks_into_table
 
 # Placeholder database connection parameters
-DB_USERNAME = "root"  # "your_db_username"
-DB_PASSWORD = "1234"  # "your_db_password"
-DB_HOST = "localhost"  # "your_db_host"
+DB_USERNAME = "root"  # Replace with your DB username
+DB_PASSWORD = "1234"  # Replace with your DB password
+DB_HOST = "localhost"  # Replace with your DB host
 DB_PORT = 1521
-DB_SID = "xe"  # "your_db_sid"
+DB_SID = "xe"  # Replace with your DB SID
+
+# Placeholder API endpoint and headers
+API_BASE_URL = "https://api.us.lawcus.com"
+API_ACCESS_TOKEN = ""  # Replace with your actual API access token
+API_HEADERS = {
+    "Authorization": f"Oauth Bearer {API_ACCESS_TOKEN}",
+    "Content-Type": "application/json",
+}
 
 
 def connect_to_database():
@@ -23,7 +31,6 @@ def connect_to_database():
 
     Replace the placeholder values with your actual database connection details.
     """
-    # dsn = oracledb.makedsn(host=DB_HOST, port=DB_PORT, sid=DB_SID)
     d = None  # default suitable for Linux
     if platform.system() == "Darwin" and platform.machine() == "x86_64":  # macOS
         d = os.environ.get("HOME") + ("/Downloads/instantclient_21_12")
@@ -35,14 +42,14 @@ def connect_to_database():
     return cursor
 
 
-def make_api_request(endpoint):
+def make_api_request(endpoint, params=None):
     """
     Make a dynamic API request based on the provided endpoint.
 
     Replace the placeholder API_BASE_URL and API_HEADERS with your actual API details.
     """
     url = f"{API_BASE_URL}/{endpoint}"
-    response = requests.get(url, headers=API_HEADERS)
+    response = requests.get(url, headers=API_HEADERS, params=params)
 
     if response.status_code == 200:
         data = response.json()
@@ -53,33 +60,50 @@ def make_api_request(endpoint):
 
 
 def get_api_token():
+    # Add your code to get the API token here
     pass
 
-
-# Placeholder API endpoint and headers
-API_BASE_URL = "https://api.us.lawcus.com"
-API_ACCESS_TOKEN = ""
-API_HEADERS = {
-    "Authorization": f"Oauth Bearer {API_ACCESS_TOKEN}",
-    "Content-Type": "application/json",
-}
 
 if __name__ == "__main__":
     # Connect to the Oracle database
     oracle_cursor = connect_to_database()
 
-    create_contact_table(oracle_cursor)
-    create_matters_table(oracle_cursor)
-    create_leads_table(oracle_cursor)
-    create_interactions_table(oracle_cursor)
-    create_tasks_table(oracle_cursor)
-    create_accounts_table(oracle_cursor)
-    # Make API request for the Contacts endpoint
-    # contacts_data = make_api_request("contacts")
+    # Define the endpoints and parameters you need to call
+    endpoints = [
+        ("contacts", None),
+        ("matters", None),
+        ("leadsources", None),
+        ("interactions",
+         {"types": '["PHONE","EMAIL","SECURE_MESSAGE"]', "sub_types": '["INBOUND","OUTBOUND"]', "skip": 0,
+          "paginate": 20}),
+        ("tasks", None),
+        ("accounts", None),
+    ]
 
-    # if contacts_data:
-    #     # Insert Contacts data into the Oracle database
-    #     insert_contacts_into_table(oracle_cursor, contacts_data["list"])
+    for endpoint, params in endpoints:
+        # Make API request for the current endpoint
+        endpoint_data = make_api_request(endpoint, params)
+
+        if endpoint_data:
+            # Execute the corresponding function based on the endpoint
+            if endpoint == "contacts":
+                create_contact_table(oracle_cursor)
+                insert_contacts_into_table(oracle_cursor, endpoint_data["list"])
+            elif endpoint == "matters":
+                create_matters_table(oracle_cursor)
+                insert_matters_into_table(oracle_cursor, endpoint_data["list"])
+            elif endpoint == "leadsources":
+                create_leads_table(oracle_cursor)
+                insert_leads_into_table(oracle_cursor, endpoint_data)
+            elif endpoint == "interactions":
+                create_interactions_table(oracle_cursor)
+                insert_interactions_into_table(oracle_cursor, endpoint_data["list"])
+            elif endpoint == "tasks":
+                create_tasks_table(oracle_cursor)
+                insert_tasks_into_table(oracle_cursor, endpoint_data)
+            elif endpoint == "accounts":
+                create_accounts_table(oracle_cursor)
+                insert_accounts_into_table(oracle_cursor, endpoint_data)
 
     # Close the database cursor when done
-    # oracle_cursor.close()
+    oracle_cursor.close()
